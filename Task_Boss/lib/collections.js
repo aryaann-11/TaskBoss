ab1655d95d5cbdae1bf54de600cd1e2e65bf028f
Aryan Chatterjee <aryanchatterjee@localhost.localdomain>
2020-11-03 23:17:18 +0530

    team creation, logout button not working

---
+++ Task_Boss/lib/collections.js
@@ -0,0 +1,7 @@
+import {Mongo} from 'meteor/mongo';
+import {Meteor} from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+//export const Tasks= new Mongo.Collection("tasks");
+export const Teams=new Mongo.Collection("teams");
+
--- Task_Boss/server/main.js
+++ Task_Boss/server/main.js
@@ -1,5 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-
+import {Teams} from '../lib/collections.js';
 Meteor.startup(() => {
   // code to run on server at startup
 });
